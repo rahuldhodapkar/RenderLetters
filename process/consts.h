3a764f1a51c742e2773911d523899084db722246
rahuldhodapkar <rahul.m.dhodapkar@gmail.com>
2015-04-27 23:13:28 -0400

    modified some stuff to make the render function take various command line input

--- process/consts.h
+++
@@ -1,9 +0,0 @@
-/** consts.h
- * holds basic constants related to resolution and size.
- */
-
-// the glyph size in pixels.
-const int GLYPH_SIZE = 80;
-
-const char * FONT_STYLE_AND_SIZE = "Arial Unicode MS 50";
-
